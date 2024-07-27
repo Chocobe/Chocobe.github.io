34303a936a047064bd26c35542d176680c281046
Chocobe <nice_10bird@naver.com>
2024-07-27 16:18:05 +0000

    Deploying to gh-pages from @ Chocobe/Chocobe.github.io@03959f89f9123f37a287d966b2f220cdea03817a 🚀

---
+++ _next/static/chunks/app/threejs-practices/page-81a0460c9df3053c.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[930],{4280:function(e,r,t){Promise.resolve().then(t.bind(t,8576))},8576:function(e,r,t){"use strict";t.r(r);var n=t(7437);function ThreejsPractices(){return(0,n.jsx)("div",{children:"Threejs Practices page"})}r.default=ThreejsPractices},622:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(2265),o=Symbol.for("react.element"),s=Symbol.for("react.fragment"),c=Object.prototype.hasOwnProperty,f=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,i={key:!0,ref:!0,__self:!0,__source:!0};function q(e,r,t){var n,s={},u=null,_=null;for(n in void 0!==t&&(u=""+t),void 0!==r.key&&(u=""+r.key),void 0!==r.ref&&(_=r.ref),r)c.call(r,n)&&!i.hasOwnProperty(n)&&(s[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===s[n]&&(s[n]=r[n]);return{$$typeof:o,type:e,key:u,ref:_,props:s,_owner:f.current}}r.Fragment=s,r.jsx=q,r.jsxs=q},7437:function(e,r,t){"use strict";e.exports=t(622)}},function(e){e.O(0,[971,472,744],function(){return e(e.s=4280)}),_N_E=e.O()}]);
